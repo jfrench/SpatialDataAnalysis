301edab2bb5884d5e06f22ecbbe6eaf2a118628d
Joshua French <joshua.french@gmail.com>
2023-09-05 15:33:48 -0600

    Updated Ch 6 code

--- ch06-case-control-point-data/ch06a-r-kd.R
+++ ch06-case-control-point-data/ch06a-r-kd.R
@@ -28,14 +28,16 @@ title("Gaussian kernel, Bandwidth = 700")
 # calculate log ratio of spatial densities for bandwidth = 350
 f350 = spdensity(grave[af, ], sigma = 350)
 g350 = spdensity(grave[-af,], sigma = 350)
-r350 = logrr(grave, case = 2, sigma = 350)
+r350 = logrr(grave, case = "affected", sigma = 350)
 # contour plot of log ratio of spatial densities
 contour(r350, lty = c(2, 1, 1, 1, 1), main = "")
 # construct 95% tolerance envelopes for log relative risk
 # when bandwidth = 350
-renv350 = logrr(grave, sigma = 350, case = 2, nsim = 999,
-                level = 0.95)
-save(renv350, file = "renv350.rda")
+if (!file.exists("renv350.rda")) {
+  renv350 = logrr(grave, sigma = 350, case = "affected",
+                  nsim = 999, level = 0.95)
+  save(renv350, file = "renv350.rda")
+}
 load("renv350.rda")
 # image plot showing regions where r350 is outside
 # tolerance envelopes
@@ -44,7 +46,8 @@ plot(renv350)
 # a better color scale (in my opinion)
 # making it easier to distguish the clusters of cases relative
 # to controls (red) and vice versa (blue)
-grad = gradient.color.scale(min(renv350$v, na.rm = TRUE), max(renv350$v, na.rm = TRUE))
+grad = gradient.color.scale(min(renv350$v, na.rm = TRUE),
+                            max(renv350$v, na.rm = TRUE))
 plot(renv350, col = grad$col, breaks = grad$breaks)
 
 ## global test that spatial densities of cases/controls
@@ -52,9 +55,12 @@ plot(renv350, col = grad$col, breaks = grad$breaks)
 logrr.test(renv350)
 
 # construct 95% tolerance envelopes for bandwidth of 700
-renv700 = logrr(grave, case = "affected", nsim = 999, sigma = 700,
-                nyx = c(60, 60), level = 0.95)
-save(renv700, file = "renv700.rda")
+if (!file.exists("renv700.rda")) {
+  renv700 = logrr(grave, case = "affected", nsim = 999,
+                  sigma = 700, nyx = c(60, 60),
+                  level = 0.95)
+  save(renv700, file = "renv700.rda")
+}
 load("renv700.rda")
 # image plot showing regions where r700 is outside tolerance
 # envelopes
@@ -63,7 +69,7 @@ plot(renv700)
 ### difference in K functions
 # estimate K function for affected and unaffected,
 # take their difference
-kd = kdest(grave, case = 2)
+kd = kdest(grave, case = "affected")
 # plot estimated and theoretical KD (under RLH)
 plot(kd, cbind(iso, theo) ~ r, legend = FALSE, main = "")
 
@@ -72,10 +78,12 @@ plot(kd, cbind(iso, theo) ~ r, legend = FALSE, main = "")
 # you shouldn't specify r unless you know what spatial scales
 # you want to consider.
 nsim = 499
-kdenv = kdest(grave, case = "affected", nsim = 499,
-              r = seq(0, 2000, len = 201),
-              level = 0.95)
-save(kdenv, file = "kdenv.rda")
+if (!file.exists("kdenv.rda")) {
+  kdenv = kdest(grave, case = "affected", nsim = 499,
+                r = seq(0, 2000, len = 201),
+                level = 0.95)
+  save(kdenv, file = "kdenv.rda")
+}
 load("kdenv.rda")
 # print some information about kdenv
 print(kdenv)
@@ -84,9 +92,11 @@ summary(kdenv)
 # plot results
 plot(kdenv, ylab = "difference in K functions",
      xlim = c(0, 2000))
-legend("topleft", legend = c("obs", "avg", "max/min env", "95% env"),
-       lty = c(1, 2, 1, 2), col = c("black", "red", "darkgrey", "lightgrey"),
-       lwd = c(1, 1, 10, 10))
+# legend("topleft",
+#        legend = c("obs", "avg", "max/min env", "95% env"),
+#        lty = c(1, 2, 1, 2),
+#        col = c("black", "red", "darkgrey", "lightgrey"),
+#        lwd = c(1, 1, 10, 10))
 
 # KD+ global test
 # H0: KD(r) = 0 for all r considered
--- ch06-case-control-point-data/ch06b-local-rates.R
+++ ch06-case-control-point-data/ch06b-local-rates.R
@@ -10,18 +10,25 @@ scan = spscan.test(grave, nsim = 999, case = "affected")
 # summary of scan test results
 summary(scan)
 # plot scan test results
-plot(scan, chars = c(1, 20), main = "most likely cluster for grave data",
+plot(scan, chars = c(1, 20),
+     main = "most likely cluster for grave data",
      border = "orange")
 # extract most likely and other significant clusters
 clusters(scan)
 
 # if there were more than 1 cluster ....
-scan2 = spscan.test(grave, nsim = 999, case = "affected", alpha = 0.8)
+scan2 = spscan.test(grave, nsim = 999, case = "affected",
+                    alpha = 0.8)
 summary(scan2)
-plot(scan2, chars = c(1, 20), main = "detected clusters for grave data",
-     border = c("blue", "orange"), clwd = c(3, 2), clty = c(2, 1))
+plot(scan2, chars = c(1, 20),
+     main = "detected clusters for grave data",
+     border = c("blue", "orange"), clwd = c(3, 2),
+     clty = c(2, 1))
 clusters(scan2)
 
 # q nearest neighbor test
 # use position in levels(grave$marks) to select case group
-qnn.test(grave, q = c(3, 5, 7, 9, 11, 13, 15), nsim = 499, case = 2)
+qnn.test(grave,
+         q = c(3, 5, 7, 9, 11, 13, 15),
+         nsim = 499,
+         case = "affected")
